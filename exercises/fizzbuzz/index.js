fb0a45e122fc24649caf8069f87af03842c44965
lukevandekieft <vandekie@gmail.com>
2020-05-11 18:56:04 -0700

    Solve fizzbuzz.

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,31 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  // init note: test for non-int values beforehand
+
+  for(let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+    } else if (i % 5 === 0 ) {
+      console.log("buzz");
+    } else {
+      console.log(i);
+    }
+  }
+
+  // init note: negatives and 0 are interesting considerations
+  // if (n >= 0) {
+  //   // count up
+  // } else {
+  //   //count down
+  // }
+}
 
 module.exports = fizzBuzz;
+
+// ********** RESULTS ************
+// my first legit pass! I MAY want to join 3 & 5 to lowest denominator
+// Remember for discussion that ternary CAN be used but is generally only advised when it's legible
