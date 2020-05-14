362055a6eb9dcaa1a987ebc703507616e72b6ab1
lukevandekieft <vandekie@gmail.com>
2020-05-13 21:46:23 -0700

    Solve pyramid.

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,17 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  // n = positive int, otherwise error handling needs to be built
+
+  // length = n*2 - 1
+  let printString = " ".repeat(n - 1) + "#" + " ".repeat(n - 1);
+  console.log(printString);
+
+  for (let i = 1; i < n; i++) {
+    printString = printString.slice(1, n-1) + "###" + printString.slice(n, 2*n - 2)
+    console.log(printString);
+  }
+}
 
 module.exports = pyramid;
