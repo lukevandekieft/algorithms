3868b030d9f6263f0dac4a66bad46084374acad2
lukevandekieft <vandekie@gmail.com>
2020-06-10 10:06:00 -0700

    Solve bst insert method.

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,37 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert (number) {
+    const placeNumberInTree = (currentNode) => {
+      if (number < currentNode.data) {
+        if (currentNode.left) {
+          placeNumberInTree(currentNode.left)
+        } else {
+          currentNode.left = new Node(number);
+        }
+      } else if (number > currentNode.data) {
+        if (currentNode.right) {
+          placeNumberInTree(currentNode.right)
+        } else {
+          currentNode.right = new Node(number);
+        }
+      } else {
+        console.log("Error, number equals node value");
+      }
+    }
+
+    placeNumberInTree(this);
+  }
+}
 
 module.exports = Node;
+
+// ****************** RESULTS **********************
+// Right idea but placeNumberInTree is overkill. Try to make insert itself recursive by running the function directly on a node (e.g. this.left.insert(data) vs. placeNumberInTree(this.left) )
