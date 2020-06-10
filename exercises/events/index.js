b4d748cd40babc74747a9cd3c350a3dd7b2f1883
lukevandekieft <vandekie@gmail.com>
2020-06-10 15:08:27 -0700

    Copied answer for events.

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,38 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  constructor() {
+    this.events = {
+
+    }
+  }
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    if (this.events[eventName]) {
+      this.events[eventName].push(callback);
+    } else {
+      this.events[eventName] = [callback];
+    }
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    if (this.events[eventName]) {
+      for (let callback of this.events[eventName]) {
+        callback();
+      }
+    }
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    delete this.events[eventName];
+  }
 }
 
 module.exports = Events;
+
+// **************** RESULTS *****************
+// I basically just skipped this one - I don't think jQuery study is relevant for me at this time and place in life.
